b1dfa5844b6c421fd403a05fe0c146da56145d05
Samuel Lemes <samuellemes25@gmail.com>
2017-09-04 21:59:17 -0300

    Creating basic structure for hello-rest EAR project.

---
+++ hello-rest/hello-rest-core/src/main/java/br/edu/univas/si8/ta/hello/rest/impl/HelloServiceImpl.java
@@ -0,0 +1,14 @@
+package br.edu.univas.si8.ta.hello.rest.impl;
+
+import br.edu.univas.si8.ta.hello.rest.api.HelloService;
+
+public class HelloServiceImpl implements HelloService {
+
+	@Override
+	public String sayHello(String name) {
+		
+		return String.format("<h1>Hello %s!!!</h1>", name);
+		
+	}
+
+}
