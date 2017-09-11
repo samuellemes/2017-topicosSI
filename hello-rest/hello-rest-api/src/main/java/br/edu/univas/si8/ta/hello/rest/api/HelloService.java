313e135692fb9881e4d137363564da0c78262ac9
Samuel Lemes <samuellemes25@gmail.com>
2017-09-11 20:27:40 -0300

    Adding a method to the rest service to sum two integers.

--- hello-rest/hello-rest-api/src/main/java/br/edu/univas/si8/ta/hello/rest/api/HelloService.java
+++ hello-rest/hello-rest-api/src/main/java/br/edu/univas/si8/ta/hello/rest/api/HelloService.java
@@ -2,9 +2,11 @@ package br.edu.univas.si8.ta.hello.rest.api;
 
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/service")
@@ -15,4 +17,8 @@ public interface HelloService {
 	@Produces(MediaType.TEXT_HTML)
 	String sayHello(@PathParam("name") String name);
 	
+	@GET
+	@Path("/sum")
+	@Produces(MediaType.APPLICATION_JSON)
+	Result sum(@QueryParam("a") int a, @QueryParam("b") int b);
 }
--- hello-rest/hello-rest-core/src/main/java/br/edu/univas/si8/ta/hello/rest/impl/HelloServiceImpl.java
+++ hello-rest/hello-rest-core/src/main/java/br/edu/univas/si8/ta/hello/rest/impl/HelloServiceImpl.java
@@ -1,6 +1,7 @@
 package br.edu.univas.si8.ta.hello.rest.impl;
 
 import br.edu.univas.si8.ta.hello.rest.api.HelloService;
+import br.edu.univas.si8.ta.hello.rest.api.Result;
 
 public class HelloServiceImpl implements HelloService {
 
@@ -9,6 +10,12 @@ public class HelloServiceImpl implements HelloService {
 		
 		return String.format("<h1>Hello %s!!!</h1>", name);
 		
+	}
+
+	@Override
+	public Result sum(int a, int b) {
+		int result = a+b;
+		return new Result().withA(a).withB(b).withValue(result);
 	}
 
 }
